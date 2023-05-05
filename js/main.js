800b3e4062b07a55fba86bddf888dafa4f2f3613
Guilherme Martins <31105633+Guilhermevsm@users.noreply.github.com>
2023-05-05 12:03:32 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
 function focar_contato(){
   fale_conosco.scrollIntoView({behavior:"smooth"});
-}
+};
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  
+  centerSlide: true,
+  fade: 'true',
+  loop: true,                         //loop
+  
+  
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 17,
+      centeredSlides: true,
+      loop: false,   
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 27,
+      loop: false,   
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+      loop: false,   
+    },
+  },
+});
